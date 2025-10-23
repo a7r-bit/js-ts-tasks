b30a70d5d0b1930fdfc320ddde1e1f065b4be7e7
Artur Matukevich <malikharik@gmail.com>
2025-10-23 13:29:59 +0300

    fix: delete comments

--- src/diffElems.js
+++ src/diffElems.js
@@ -4,17 +4,6 @@
  * @returns {number}
  */
 module.exports.diffElems = function diffElems(arr) {
-  /*Fitrs solution
   const set = new Set(arr);
-  return set.size
-  */
-
-  // Second solution
-  let diffNumberCount = 0;
-  const sortedArr = arr.sort((a, b) => a - b);
-
-  for (let i = 0; i < sortedArr.length; ++i) {
-    if (sortedArr[i] != sortedArr[i - 1]) diffNumberCount++;
-  }
-  return diffNumberCount;
+  return set.size;
 };
